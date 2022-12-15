656f24e2bbcd014a76eb88b8c320f669d779a967
Moamin <moamin.salamah@gmail.com>
2022-12-15 14:11:26 +0100

    1st commit

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/shop-context";
+import CartItem from "./cartItem";
+import { useNavigate } from "react-router-dom";
+import "./cart.css";
+
+const Cart = () => {
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {PRODUCTS.map((product) =>{
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} key={product.id} />;
+          }
+        })}
+      </div>
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: {totalAmount} €</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        <h1> Your Cart is Empty</h1>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
